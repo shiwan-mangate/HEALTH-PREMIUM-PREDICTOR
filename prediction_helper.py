1b6eaa65d06f588302946ed4a9e84e3885065538
shiwan-mangate <mangateshiwan@gmail.com>
2025-07-19 11:23:21 +0530

    Update prediction_helper.py

--- prediction_helper.py
+++ prediction_helper.py
@@ -1,11 +1,11 @@
-from joblib import load
+import joblib
 import pandas as pd
 
-model_rest=load("model_rest.joblib")
-model_young=load("model_young.joblib")
+model_rest=joblib.load("model_rest.joblib")
+model_young=joblib.load("model_young.joblib")
 
-scaler_rest=load("scaler_rest.joblib")
-scaler_young=load("scaler_young.joblib")
+scaler_rest=joblib.load("scaler_rest.joblib")
+scaler_young=joblib.load("scaler_young.joblib")
 
 def calculate_normalized_risk(medical_history):
     risk_scores = {
